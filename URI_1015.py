3ae60cf2186d2845bea64d39f4967e7b8a3330df
Jorge Júnior <jorge.limajr@outlook.com>
2020-12-20 23:29:08 -0300

    Distância Entre Dois Pontos

---
+++ URI_1015.py
@@ -0,0 +1,13 @@
+from math import sqrt, pow
+
+x1, y1 = input().split()
+x1 = float(x1)
+y1 = float(y1)
+
+x2, y2 = input().split()
+x2 = float(x2)
+y2 = float(y2)
+
+d = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
+
+print(f'{d:.4f}')
