b05ba38629071cb92b6b0950ba677a21cc0023ba
Jorge Júnior <jorge.limajr@outlook.com>
2020-12-22 01:37:41 -0300

    Sort Simples

---
+++ URI_1042.py
@@ -0,0 +1,11 @@
+n1, n2, n3 = map(int, input().split())
+lista = [n1, n2, n3]
+lorder = sorted(lista)
+
+for i in lorder:
+    lorder = i
+    print(lorder)
+print()
+for j in lista:
+    lista = j
+    print(lista)
